68c2d12e6fe568bf265cac5917c34ba4b1252998
AMIT COHEN SABBAN <amitcohensaban@gmail.com>
2023-05-08 10:26:13 +0300

    header,initial settings

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { SignupPageComponent } from './components/auth/signup-page/signup-page.component';
+import { SignupFormComponent } from './components/auth/signup-page/signup-form/signup-form.component';
+import { LoginPageComponent } from './components/auth/login-page/login-page.component';
+import { LoginFormComponent } from './components/auth/login-page/login-form/login-form.component';
+import { AllBooksComponent } from './components/books/all-books/all-books.component';
+import { MyBooksComponent } from './components/books/my-books/my-books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SignupPageComponent,
+    SignupFormComponent,
+    LoginPageComponent,
+    LoginFormComponent,
+    AllBooksComponent,
+    MyBooksComponent
   ],
   imports: [
     BrowserModule,
