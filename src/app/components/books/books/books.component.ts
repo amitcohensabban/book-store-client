33fc756b8ea97f9576ee4d86b0e50547c9eca50b
AMIT COHEN SABBAN <amitcohensaban@gmail.com>
2023-05-15 17:39:12 +0300

    cart-icon

--- src/app/components/books/books/books.component.ts
+++ src/app/components/books/books/books.component.ts
@@ -1,34 +1,33 @@
-import { Component,Input, OnInit,Output,EventEmitter } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
 import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-books',
   templateUrl: './books.component.html',
-  styleUrls: ['./books.component.scss']
+  styleUrls: ['./books.component.scss'],
 })
-export class BooksComponent implements OnInit{
-  @Input() books:any[]
+export class BooksComponent implements OnInit {
+  @Input() books: any[];
   filteredBooks: any[];
   searchTerm: string;
   p: number = 1;
   selectedBook: any;
-  @Output() bookAdded = new EventEmitter<{ userId: string, bookId: string }>(); // new EventEmitter property
+  @Output() bookAdded = new EventEmitter<{ userId: string; bookId: string }>(); // new EventEmitter property
 
-   ngOnInit(): void {
+  ngOnInit(): void {
     setTimeout(() => {
-          this.filteredBooks = this.books;
-
-    },1200)
+      this.filteredBooks = this.books;
+    }, 1200);
   }
 
-  constructor(private auth:AuthService) {
+  constructor(private auth: AuthService) {
     this.books = [];
     this.filteredBooks = [];
     this.searchTerm = '';
   }
 
   filterBooks(): void {
-    this.filteredBooks = this.books.filter(book =>
+    this.filteredBooks = this.books.filter((book) =>
       book.title.toLowerCase().includes(this.searchTerm.toLowerCase())
     );
     console.log(this.filteredBooks);
@@ -38,15 +37,16 @@ export class BooksComponent implements OnInit{
     console.log('selectedBook:', this.selectedBook);
     const userEmail = localStorage.getItem('userName');
     try {
-      const userId:any = await this.auth.getUserIdByEmail(userEmail).toPromise();
+      const userId: any = await this.auth
+        .getUserIdByEmail(userEmail)
+        .toPromise();
       console.log('response from getUserIdByEmail:', userId.userId);
 
-      this.bookAdded.emit({ userId:userId.userId, bookId });
+      this.bookAdded.emit({ userId: userId.userId, bookId });
     } catch (error) {
       console.log(this.bookAdded);
 
       console.error(error);
     }
   }
-
 }
